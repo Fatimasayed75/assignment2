eb764466c021458e0f076167afa92770adbb94eb
xlvk <fatima77614@gmail.com>
2024-11-25 17:32:13 +0300

    add(data_display): displayed the data of the records in the index page.

--- index.php
+++ index.php
@@ -12,6 +12,29 @@ $records = include 'app.php';
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
 </head>
 <body>
-    
+    <br>
+    <br>
+    <h1 style="text-align: center;">University of Bahrain Students Enrollment by Nationality</h1>
+    <br>
+    <table>
+        <thead>
+            <tr>
+                <?php if (!empty($records)): ?>
+                    <?php foreach ($records[0] as $field => $value): ?>
+                        <th><?php echo htmlspecialchars($field); ?></th>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($records as $record): ?>
+                <tr>
+                    <?php foreach ($record as $value): ?>
+                        <td><?php echo htmlspecialchars($value); ?></td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
